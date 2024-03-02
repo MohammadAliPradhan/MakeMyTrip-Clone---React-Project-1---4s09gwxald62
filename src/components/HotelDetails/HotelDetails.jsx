08f93dbf8f20a6dac776962bde90b71041c21180
MOhammad Ali <mdsali914@gmail.com>
2024-03-02 08:39:50 -0800

    updated

--- src/components/HotelDetails/HotelDetails.jsx
+++ src/components/HotelDetails/HotelDetails.jsx
@@ -9,61 +9,67 @@ import { CropSquareSharp } from '@mui/icons-material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendar, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import FindMembers from '../FindMembers/FindMembers'
+import Airports from '../Modals/Airports';
+
 
 
 function HotelDetails() {
+    const [FlightDepartureData, setFlightDepartureData] = useState(Airports);
 
-    const { ApiInfo, setApiInfo } = useContext(ApiDetails)
     const [hotelName, setHotelName] = useState({
         destination: "Delhi",
         checkIn: 6
     })
+
+
+    //This is for city or Location Modal
+    const [modalInput, setModalInput] = useState(false);
+
+
+    // This is for calendar modal
+    const [trthly, setTrthly] = useState(true);
+    const [checkInTrthly,setCheckInTrthly] = useState(true);
+
+
+    //THis is for Guests Modal 
     const [findMemberModal, setFindMemberModal] = useState();
 
     const [MemberValue, setMemberValue] = useState()
 
+    const checkInValue = new Date('Wed Nov 15 2023 00:00:00 GMT+0530');
+    const checkOutValue = new Date('Thu Nov 17 2023 00:00:00 GMT+0530')
 
-    const [calendarState, setCalendarState] = useState({
-        checkInState: false,
-        checkOut: false,
-    })
-    const initialDate = new Date('Wed Nov 15 2023 00:00:00 GMT+0530');
-
-    const [selectedDate, setSelectedDate] = useState(initialDate)
-    const [selectedDateCheckOut, setSelectedDateCheckOut] = useState(initialDate)
+    const [checkInDate, SetcheckInDate] = useState(checkInValue)
+    const [checkOutDate, SetCheckOutDate] = useState(checkOutValue)  
+    
+    
     const navigate = useNavigate()
+
+    
 
     const [submittedHotelDetails, setSubmittedHotelDetails] = useState("");
     const [flag, setFlag] = useState(false);
 
-    //This one is the first one change of the input
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
-        setCalendarState((prevState) => ({
-            ...prevState,
-            checkInState: false
-        }));
-    };
+    
 
     const handleDayClick = (value, event) => {
         console.log('Clicked day:', value);
     };
 
-    console.log(hotelName.destination);
-
-    // This is the second second change of value
-
+    //This one is the first one change of the input
     const handleDateChangeCheckOut = (date) => {
-        setSelectedDateCheckOut(date);
-        setCalendarState((prevState) => ({
-            ...prevState,
-            checkOut: false
-        }));
+        SetCheckOutDate(date);
+        setTrthly(true);
     };
 
-    const handleDayClickCheckOut = (value, eventc) => {
-        console.log(value);
-    }
+
+    //This is checkinDate onchange function
+    const handleDateCheckIn = (date) => {
+        SetcheckInDate(date);
+        setCheckInTrthly(true);
+    };
+
+  
 
     function handleBookingvalue(findmembers) {
         setMemberValue(findmembers)
@@ -92,16 +98,7 @@ function HotelDetails() {
 
 
     };
-
-
-    function handleOnCick(e, field) {
-        const { value } = e.target;
-        setHotelName((oldState) => ({
-            ...oldState,
-            [field]: value
-        }))
-    }
-
+    
     useEffect(() => {
         getHotelDetails(submittedHotelDetails)
     }, [submittedHotelDetails]);
@@ -111,24 +108,71 @@ function HotelDetails() {
         setSubmittedHotelDetails(hotelName.destination)
     }
 
-    function handleInputClick() {
-        setCalendarState((oldState) => ({
-            ...oldState,
-            checkInState: !oldState.checkInState
-        }));
+
+
+       //creating date variable for checkIn
+    
+       const alldatecheckIn = checkInDate.toISOString();
+
+       const yearcheckin = alldatecheckIn.substring(2, 4);
+       const daycheckin = alldatecheckIn.substring(8, 10);
+   
+       const dayWeekcheckin = checkInDate.toDateString().substring(0,4);
+       const monthWordcheckin = checkInDate.toDateString().substring(4,7);
+       //end date
+
+
+
+
+        //creating date variable for checkOut
+    
+        const alldatecheckOut = checkOutDate.toISOString();
+       
+        const yearcheckout = alldatecheckOut.substring(2, 4);
+        const daycheckout = alldatecheckOut.substring(8, 10);
+          
+        const dayWeekcheckout = checkOutDate.toDateString().substring(0,4);
+        const monthWordcheckout = checkOutDate.toDateString().substring(4,7);
+        //end date
+
+       //getHotelData
+
+       function handleGetCityName(event) {
+        const { value } = event.target;
+        let filteredSearch = Airports.filter((data) => {
+            let code = data.IATA_code.toLowerCase();
+            let airportName = data.airport_name.toLowerCase();
+            let cityName = data.city_name.toLowerCase();
+            let result = value.toLowerCase();
+            if (code.includes(result) || airportName.includes(result) || cityName.includes(result)) {
+                return data
+            }
+        })
+        setFlightDepartureData(filteredSearch);
     }
 
-    function handleInputClickTwo() {
-        setCalendarState((oldState) => ({
-            ...oldState,
-            checkOut: !oldState.checkOut
-        }));
+    function valuesetter(val) {
+        console.log(val);
+        if (val) {
+            setHotelName((oldState) => ({
+                ...oldState,
+                destination: val,
+            }))
+        } else {
+            setHotelName((oldState) => ({
+                ...oldState,
+                from: AirportData[0].city_name,
+            }))
+        }
+        setModalInput(false);
     }
 
 
+    //End HotelData
 
 
-    console.log(calendarState.checkInState);
+
+
 
     const totalMembers = parseInt(MemberValue?.kids || 0, 10) + parseInt(MemberValue?.adult || 0, 10);
 
@@ -163,84 +207,119 @@ function HotelDetails() {
                             <button className="searchBtn" type="submit">Search</button>
                         </div>
                     </form>
-                    <div className="flight-search">
-                        <div className="flight">
-                            <span>
-                                City Property Name or Location
-                            </span>
-                            <h1><input
-                                type="text"
-                                className='something'
-                                onChange={(e) => handleOnCick(e, 'destination')}
-                                value={hotelName.destination}
-                            /></h1>
-                            <span>{hotelName.destination}, {hotelName.destination} Airport India</span>
+
+                    <section className="hotel-booking-details-container booking-details-container">
+                       
+                        <div   style={{position: "relative"}}>
+                            <label onClick={() => setModalInput(!modalInput)} htmlFor="location" className='booking-inputBox'>
+                                <span className='from-span'>City Or Location</span>
+                                <input   className='from-div-input' type="text" readOnly id='fromCity' value={hotelName.destination} style={{caretColor: "transparent"}}/>
+                                <span className='from-span'>India</span>
+                            </label>
+                            {modalInput && <modal className="modal-flight-for">
+                                <div className="modal-flight-for-input">
+                                    <img className="search-icon-img" src="	https://upload.wikimedia.org/wikipedia/commons/c/ca/VisualEditor_-_Icon_-_Search.svg" alt="" />
+                                    <input onChange={handleGetCityName} type="text" placeholder="From" />
+                                </div>
+
+                                <ul className="modal-flight-for-ul">
+                                    {FlightDepartureData && FlightDepartureData.map((details) => (
+                                        <li onClick={() => valuesetter(details.city_name)} className="modal-flight-for-li">
+                                            <div className="modal-flight-for-div">
+                                                <span className="modal-flight-for-span">{details.IATA_code}</span>
+                                            </div>
+
+                                            <div>
+                                                <span className="modal-flight-for-span-two" >{details.city_name}</span>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </modal>}
                         </div>
 
-                        <div className="flight">
-                            <span>
-                                Check In
-                            </span>
-
-                            {/* //do the work */}
-                            {calendarState.checkInState ? < div >
-                                {/* <span onClick={() => setCalendarState(!calendarState.checkInState)} className='calendarCross-Hotel'>Done</span> */}
-                                <Calendar className="calendarOn" onChange={handleDateChange} onClickDay={handleDayClick} value={selectedDate} minDate={new Date()} />
-                            </div> : <h1><input
-                                type="text"
-                                className='something'
-                                value={selectedDate.toDateString()}
-                                onClick={handleInputClick}
-
-                            /></h1>}
-
-
-                            <span onClick={handleInputClick}><FontAwesomeIcon icon={faCalendar} /></span>
-                        </div>
-
-                        <div className="flight">
-                            <span>
-                                Check Out
-                            </span>
-
-                            {/* do the work two */}
-                            {calendarState.checkOut ? <div >
-                                {/* <span onClick={() => setCalendarState(!calendarState.checkOut)} className='calendarCrossTwo-hotel'>Done</span> */}
-                                <Calendar className="calendarOn" onChange={handleDateChangeCheckOut} onClickDay={handleDayClickCheckOut} value={selectedDateCheckOut} minDate={new Date(selectedDate)} />
-                            </div> : <h1><input
-
-                                type="text"
-                                className='something'
-                                value={selectedDateCheckOut.toDateString()}
-                                onClick={handleInputClickTwo}
-
-                            /></h1>}
-                            <span><FontAwesomeIcon icon={faCalendar} /></span>
-                        </div>
-
-                        <div className="flight" >
-                            <div onClick={() => setFindMemberModal(!findMemberModal)}>
-                                <span>
-                                    Members
+                        <div onClick={() => setCheckInTrthly(!checkInTrthly)} style={{position: "relative" , borderLeft: "1px solid #e7e7e7"}}>
+                            <label htmlFor="checkIn" className='booking-inputBox'>
+                                <span className='dropdown'>
+                                    Check-In
                                 </span>
-                                <h3 className='inputHeremains'>Members:{totalMembers} </h3>
-                            </div>
 
-                            {findMemberModal && <div className='findMemberssomething'><FindMembers onBookingValue={handleBookingvalue} /></div>}
+                                {checkInTrthly ? <><div style={{fontSize: "30px", lineHeight: "36px"}}>
+                                       <span style={{fontWeight: "900", paddingRight:"6px"}}>{daycheckin}</span> 
+                                       <span style={{fontSize: "20px"}}>{monthWordcheckin}</span>
+                                       <span  className='shortYear'>{yearcheckin}</span>
+                               </div>
 
-
-                            <span>{MemberValue === undefined ? "7" : MemberValue?.adult} Adult, {MemberValue === undefined ? "7" : MemberValue?.kids} Kids ({MemberValue === undefined ? 6 : totalMembers} Members)</span>
+                                <span>
+                                    {dayWeekcheckin}
+                                </span>
+                                </>
+                                
+                                : <div onClick={(e)=>e.stopPropagation()}><Calendar className="calendarOnOfFlight" onChange={handleDateCheckIn} onClickDay={handleDayClick} value={checkInDate} minDate={new Date()} /></div>}
+                            </label>
                         </div>
 
 
-                    </div>
+                        <div onClick={()=>setTrthly(!trthly)} style={{position: "relative" , borderLeft: "1px solid #e7e7e7"}}>
+                            <label htmlFor="travelDate" className='booking-inputBox'>
+                               
+                                
+                                <span className='dropdown'>
+                                    Check-Out
+                                </span>
 
-                    {/* <div className='searchParent'>
+                                {trthly ?<> <div style={{fontSize: "30px", lineHeight: "36px"}}>
+                                       <span style={{fontWeight: "900", paddingRight:"6px"}}>{daycheckout}</span> 
+                                       <span style={{fontSize: "20px"}}>{monthWordcheckout}</span>
+                                       <span  className='shortYear'>{yearcheckout}</span>
+                               </div>
 
-                        <div className='searchBtn'>
-                            SEARCH
+                                <span>
+                                    {dayWeekcheckout}
+                                </span>
+
+                                </>
+                                
+                                : <div onClick={(e)=>e.stopPropagation()}><Calendar className="calendarOnOfFlight" onChange={handleDateChangeCheckOut} onClickDay={handleDayClick} value={checkOutDate} minDate={new Date()} /></div>}
+                            </label>
                         </div>
-                    </div> */}
+
+                        <div onClick={()=>setFindMemberModal(!findMemberModal)} style={{position: "relative" , borderLeft:  "1px solid #e7e7e7"}}>
+                            <label htmlFor="class" className='booking-inputBox-last'>
+                                <span className='dropdown'>
+                                    Guests
+                                </span>
+
+                                <div style={{ fontSize: "30px", lineHeight: "36px"}}>
+                                    <span style={{fontWeight: 900, marginRight: "10px"}}>{MemberValue === undefined ? "1" : MemberValue?.adult}</span>
+                                    <span style={{fontSize: "20px",  lineHeight: "36px",marginRight: "10px"}}>Adults</span>
+                                    <span style={{fontWeight: 900, marginRight: "10px"}}>{MemberValue === undefined ? "1" : MemberValue?.kids}</span>
+                                    <span style={{fontSize: "20px",  lineHeight: "36px"}}> Kids </span>
+                                </div>
+
+                                <span className='from-span'>People</span>
+                            </label>
+                                {findMemberModal && <div className='flightadult'>
+                                    <div onClick={(e)=> e.stopPropagation()} className='findMemberssomething'><FindMembers onBookingValue={handleBookingvalue} /></div>
+                                    </div>}
+
+                        </div>
+
+
+                        <div style={{borderLeft: "1px solid #e7e7e7"}}>
+                             <label style={{cursor: "not-allowed"}} htmlFor="price" className='booking-inputBox'>
+                                <span className='dropdown'>
+                                    Price Per Night
+                                </span>
+
+                                <span className='span-priceHotel'>₹0-₹1500, ₹1500-₹2500,...</span>
+                             </label>
+                        </div>
+
+                    </section>
+                  
+
+                  
 
 
 
